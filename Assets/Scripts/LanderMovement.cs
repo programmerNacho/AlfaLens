218d24678d361004cd8c4f4b174c111441dbeee1
Ignacio <ignacio.programmer@gmail.com>
2020-09-28 23:38:31 +0200

    Astronaut implemented.

---
+++ Assets/Scripts/LanderMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanderMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float distanceFromCamera = 1f;
+    [SerializeField]
+    private float smoothTime = 5f;
+
+    private Camera mainCamera;
+    private bool canMove;
+
+    private Vector3 positionBeforeMoving;
+
+    public bool CanMove
+    {
+        get
+        {
+            return canMove;
+        }
+
+        set
+        {
+            canMove = value;
+            if(canMove)
+            {
+                positionBeforeMoving = transform.position;
+            }
+            else
+            {
+                transform.position = positionBeforeMoving;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        canMove = false;
+    }
+
+    private void Update()
+    {
+        if(canMove)
+        {
+            Ray cameraRay = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, mainCamera.transform.position.z));
+            Vector3 position = cameraRay.GetPoint(distanceFromCamera);
+            transform.position = Vector3.Lerp(transform.position, position, smoothTime * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchBehaviour : MonoBehaviour
+{
+    public enum States { Off, On }
+
+    [SerializeField]
+    private States currentState;
+    public UnityEvent OnTurnOn = new UnityEvent();
+    public UnityEvent OnTurnOff = new UnityEvent();
+
+    public void Change()
+    {
+        switch (currentState)
+        {
+            case States.Off:
+                currentState = States.On;
+                break;
+            case States.On:
+                currentState = States.Off;
+                break;
+        }
+        StateChangeCallEvents();
+    }
+
+    public void Change(States state)
+    {
+        currentState = state;
+        StateChangeCallEvents();
+    }
+
+    private void StateChangeCallEvents()
+    {
+        switch (currentState)
+        {
+            case States.Off:
+                OnTurnOff.Invoke();
+                break;
+            case States.On:
+                OnTurnOn.Invoke();
+                break;
+            default:
+                break;
+        }
+    }
+}
